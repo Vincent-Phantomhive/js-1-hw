1d32bd9d04ca1a7e7fb6f5d98fffb5f4a5b61ff7
Vincent-Phantomhive <126806074+Vincent-Phantomhive@users.noreply.github.com>
2023-03-29 14:41:35 +0300

    задание 4

--- dist/script.js
+++ dist/script.js
@@ -1,11 +1,14 @@
 // задание 1
-var name = prompt('Введите своё имя:');
+var name = prompt('Задание 1. Введите своё имя:');
 alert('Привет, ' + name);
 // задание 2
 var year = 2023;
-var borned = prompt('Введите дату рождения:');
+var borned = prompt('Задание 2. Введите дату рождения:');
 var old = year - borned;
 alert('Вам ' + old);
 // задание 3
-var side = prompt('Введите длину стороны квадрата:');
-alert('Периметр равен' + (Math.pow(side, 2)));
+var side = prompt('Задание 3. Введите длину стороны квадрата:');
+alert('Периметр равен ' + (side * 4));
+// задание 4
+var radius = prompt('Задание 4. Введите радиус окружности:');
+alert('Площадь окружности равна ' + (Math.pow(radius, 2) * 3.14));
