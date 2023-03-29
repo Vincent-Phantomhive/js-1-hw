15620a3959e200b2c84aecc4b2c48e870bbe687d
Vincent-Phantomhive <126806074+Vincent-Phantomhive@users.noreply.github.com>
2023-03-29 14:30:10 +0300

    1-3 задания

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,11 @@
+// задание 1
 var name = prompt('Введите своё имя:');
 alert('Привет, ' + name);
+// задание 2
+var year = 2023;
+var borned = prompt('Введите дату рождения:');
+var old = year - borned;
+alert('Вам ' + old);
+// задание 3
+var side = prompt('Введите длину стороны квадрата:');
+alert('Периметр равен' + (Math.pow(side, 2)));
--- script.ts
+++ script.ts
@@ -1,2 +1,19 @@
+// задание 1
+
 let name = prompt('Введите своё имя:');
 alert('Привет, ' + name);
+
+// задание 2
+
+const year = 2023;
+let borned = prompt('Введите дату рождения:');
+let old = year - borned;
+alert('Вам ' + old);
+
+// задание 3
+
+let side = prompt('Введите длину стороны квадрата:');
+alert('Периметр равен ' + (side**2))
+
+// задание 4
+
